df18ac712fd7062d140e28f33ac000a885e85c06
MayaCoro <mayascoro@gmail.com>
2018-06-08 23:06:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
-# dog.rb
+
+class Dog 
+  def name=(dog_name)
+    @this_dog_name = dog_name
+  end
+  def name
+    @this_dog_name
+  end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+puts "The dog's name is #{lassie.name}."
